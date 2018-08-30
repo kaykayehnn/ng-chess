b4087cf6bee59fd89bf773603552d11b5996dc2d
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2018-08-30 16:58:24 +0300

    Add gzip compression

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const compression = require('compression')
 
 const decodeUser = require('../middleware/decodeUser')
 const attachServices = require('../middleware/attachServices')
 
 module.exports = (app, database, cache) => {
+  app.use(compression())
   app.use(express.json())
   app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'))
   app.use(express.static(path.join(__dirname, '../../client/dist/ng-chess/')))
