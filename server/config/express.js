94027796211646bb24e604f27b612a0cbe0e669a
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-20 21:08:56 +0200

    Add CORS middleware

--- server/config/express.js
+++ server/config/express.js
@@ -2,11 +2,13 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const compression = require('compression')
+const cors = require('cors')
 
 const decodeUser = require('../middleware/decodeUser')
 const attachServices = require('../middleware/attachServices')
 
 module.exports = (app, database, cache) => {
+  app.use(cors())
   app.use(compression())
   app.use(express.json())
   app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'))
