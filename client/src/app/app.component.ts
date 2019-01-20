e0a4b43c6393ee5608b0f8d6f98424c9b468ff27
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-01-20 23:49:59 +0200

    Fix style

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
   title = 'Ng Chess';
+
   ngOnInit (): void {
   }
 }
