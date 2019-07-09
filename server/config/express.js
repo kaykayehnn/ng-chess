7acaf315093382327a34777e89a1c80e90795997
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-07-09 23:45:18 +0300

    Make CORS origin stricter

--- server/config/express.js
+++ server/config/express.js
@@ -8,7 +8,7 @@ const decodeUser = require('../middleware/decodeUser')
 const attachServices = require('../middleware/attachServices')
 
 module.exports = (app, database, cache) => {
-  app.use('/api', cors())
+  app.use('/api', cors({ origin: 'https://ng-chess.js.org' }))
   app.use(compression())
   app.use(express.json())
   app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'))
