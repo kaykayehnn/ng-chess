d9b566df0e8afbd99c4f1afffed161ce073f71c9
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-07-11 01:23:40 +0300

    Add helmet and a strict CSP

--- server/config/express.js
+++ server/config/express.js
@@ -3,11 +3,37 @@ const path = require('path')
 const morgan = require('morgan')
 const compression = require('compression')
 const cors = require('cors')
+const helmet = require('helmet')
 
 const decodeUser = require('../middleware/decodeUser')
 const attachServices = require('../middleware/attachServices')
 
 module.exports = (app, database, cache) => {
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          connectSrc: [
+            "'self'",
+            'ng-chess.herokuapp.com',
+            'www.google-analytics.com'
+          ],
+          scriptSrc: [
+            "'self'",
+            'www.googletagmanager.com',
+            // Gtag loads analytics from here
+            'www.google-analytics.com'
+          ],
+          imgSrc: ['*'],
+          styleSrc: ["'self'", "'unsafe-inline'", 'fonts.googleapis.com'],
+          fontSrc: ["'self'", 'fonts.gstatic.com'],
+          // Analytics load 1x1px gifs for smaller requests.
+          imgSrc: ["'self'", 'www.google-analytics.com']
+        }
+      }
+    })
+  )
   app.use('/api', cors({ origin: 'https://ng-chess.js.org' }))
   app.use(compression())
   app.use(express.json())
